4c2f6cba243d3d77d6a8df7e6aa5dab6928a37a9
Mohnish Devadiga <mohnish226@yahoo.co.in>
2019-07-19 17:48:38 +1000

    Keys Changed or removed

--- harvestor/authentification3.py
+++ harvestor/authentification3.py
@@ -1,10 +1,10 @@
 
 from tweepy import OAuthHandler
 
-ACCESS_TOKEN = "981533044522627072-YFGoc8FBwK6pqOXc5teJhFnPcu3ghtg"
-ACCESS_TOKEN_SECRET = "yXc72kyxq8F5EqtDacD1Kdneddm1XnfS1u57uS3tVyBmx"
-CONSUMER_KEY = "c67rYQVxBfyQAqrN9oMes59PB"
-CONSUMER_SECRET = "ougb9ujqYgT1AJ1VtcsjVvpjfKTkqj0IljTrcgZBTp4kgbDw4M"
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
 
 
 def getAuth(self):
